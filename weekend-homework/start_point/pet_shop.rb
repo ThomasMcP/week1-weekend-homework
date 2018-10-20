d732af496b0f8ff4d620e42c3ee8b40b52312f76
Thomas McPhee <mcp.thom@gmail.com>
2018-10-20 16:38:34 +0100

    created all functions, and uncommented pet_shop_spec functions. Fully finished and working.

--- weekend-homework/start_point/pet_shop.rb
+++ weekend-homework/start_point/pet_shop.rb
@@ -46,3 +46,51 @@ def find_pet_by_name(shop, pet_name)
   pet_details = nil if pet_details.length == 0
   return pet_details
 end
+
+def remove_pet_by_name(shop, pet_name)
+  for pet in shop[:pets]
+   shop[:pets].delete(pet) if pet[:name] == pet_name
+  end
+end
+
+def stock_count(shop)
+  return shop[:pets].length
+end
+
+def add_pet_to_stock(shop, newest_pet)
+  shop[:pets] << newest_pet
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].length
+end
+
+def add_pet_to_customer(customer, add_pet)
+  customer[:pets].push(add_pet)
+end
+
+def customer_can_afford_pet(customer, newest_pet)
+  if customer[:cash] >= newest_pet[:price]
+    return true
+  else return false
+  end
+end
+
+def sell_pet_to_customer(shop, pet, customer)
+  return p "Pet not found" if pet == nil
+  if customer_cash(customer) < pet[:price]
+    return  p "insufficient funds"
+  end
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(shop, 1)
+  remove_customer_cash(customer, pet[:price])
+  add_or_remove_cash(shop, pet[:price])
+end
